656094f7c5390b0eb5dd2e5a337bb069a013c79c
Lucas Honda <lucas.honda@innovareti.com.br>
2018-10-29 09:48:45 -0300

    full project

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
+import Home from './views/Home.vue'
+import Article from './views/Article'
+import CreateArticle from './views/CreateArticle'
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: "history",
+    routes: [{
+        path: '/login',
+        component: Login
+    }, {
+        path: '/signup',
+        component: Signup
+    }, {
+        path: '/',
+        component: Home
+    }, {
+        path: '/article/:id',
+        component: Article
+    }, {
+        path: '/articles/create',
+        component: CreateArticle
+    }]
+})
+
+export default router;
